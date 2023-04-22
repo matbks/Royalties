db15bc19470a46d20e39927fbe601ff36adac829
Backhaus <44726485+matbks@users.noreply.github.com>
2023-04-22 17:51:39 -0300

    MassDischargeBalance

--- zroyalties/webapp/controller/RedwareTools/ValueHelp/BadiOnClose.js
+++ zroyalties/webapp/controller/RedwareTools/ValueHelp/BadiOnClose.js
@@ -2,8 +2,32 @@ sap.ui.define(function () {
   "use strict";
   return {
     run: function (oEvent, oSelectedItem, oParentView) {
+      debugger;
+      var sBalancePath = "/ZADOC_ROYALTIES_BALANCE_SUM"; 
+      var oBalaceFilter = new sap.ui.model.Filter(
+        "Partner",
+        sap.ui.model.FilterOperator.EQ,
+        oSelectedItem.mProperties.title
+      );
+      var oBalanceInput = oEvent
+      .getSource()
+      .getParent()
+      .byId("Balance");
+      debugger;
 
-     
+      oModel.read(sBalancePath, {
+        filters: [oBalaceFilter],
+
+        success: function (oData, oResponse) { 
+ 
+          oBalanceInput = oData.results[1].Quantity; 
+          
+        }.bind(this),
+
+        error: function (oError) { 
+          sap.m.MessageToast.show("Erro ao carregar os dados de transgenia");
+        },
+      });
      
  
     },
