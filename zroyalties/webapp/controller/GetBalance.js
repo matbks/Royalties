8883607fdd469b19341f050625035ebaed4713fb
Backhaus <44726485+matbks@users.noreply.github.com>
2023-04-23 21:15:40 -0300

    MassDischargeBalanceCLass

---
+++ zroyalties/webapp/controller/GetBalance.js
@@ -0,0 +1,58 @@
+sap.ui.define(["sap/ui/base/ManagedObject"], function (ManagedObject) {
+  "use strict";
+
+  return ManagedObject.extend("royalties.zroyalties.controller.GetBalance", {
+    constructor: function (Type, Value, oInput, oParentView) {
+      this.getBalance(Type, this.AlphaIn(Type, Value), oInput, oParentView);
+    },
+
+    AlphaIn: function (Type, RawValue) {
+      var ValueOriginalLength;
+      if (Type == "Contrato") ValueOriginalLength = "10";
+      else if (Type == "Parceiro") ValueOriginalLength = "10";
+      if (RawValue.length < ValueOriginalLength) {
+        var MissingLeadingZeros = ValueOriginalLength - RawValue.length;
+        for (var i = 0; i < MissingLeadingZeros; i++) {
+          RawValue = "0" + RawValue;
+        }
+        return RawValue;
+      }
+    },
+
+    getBalance: function (Type, Value, oInput) {
+      var Path;
+      if (Type === "Parceiro") {
+
+        var oFilter = new sap.ui.model.Filter(
+          "Partner",
+          sap.ui.model.FilterOperator.EQ,
+          Value
+        );
+
+        Path = "/ZADOC_ROYALTIES_BALANCE";
+
+      } else if (Type === "Contrato") {
+
+        var oFilter = new sap.ui.model.Filter(
+          "Contract",
+          sap.ui.model.FilterOperator.EQ,
+          Value
+        );
+
+        Path = "/ZADOC_ROYALTIES_BALANCE_C";
+      }
+      var oModel = oParentView.getModel();
+      oModel.read(Path, {
+        filters: [oFilter],
+        success: function (oData, oResponse) {
+          if (oData.results[0]) {
+            oInput.setValue(oData.results[0].Balance);
+          }
+        }.bind(this),
+        error: function (oError) {
+          sap.m.MessageToast.show("Erro ao carregar o saldo");
+        },
+      });
+    },
+  });
+});
--- zroyalties/webapp/controller/RedwareTools/ValueHelp/BadiOnClose.js
+++ zroyalties/webapp/controller/RedwareTools/ValueHelp/BadiOnClose.js
@@ -1,57 +1,59 @@
-sap.ui.define(function () {
-  "use strict";
-  return {
-    run: function (oEvent, oSelectedItem, oParentView) {
-      if (oSelectedItem) {
-        var oModel = oParentView.getModel();
+sap.ui.define(
+  ["royalties/zroyalties/controller/GetBalance"],
+  function (GetBalance) {
+    "use strict";
+    return {
+      run: function (oEvent, oSelectedItem, oParentView) {
+        if (oSelectedItem) {
+          debugger;
+          var oBalanceInput = oEvent.getSource().getParent().byId("Balance");
+          var Type =  oEvent.getSource().getTitle();
+          new GetBalance(
+            Type,
+            oSelectedItem,
+            oBalanceInput,
+            oParentView
+          );
 
-        var Contract = oParentView.byId("Contract").getValue();
+          // var oModel = oParentView.getModel();
 
-        var Partner = oSelectedItem.mProperties.title;
+          // var Contract = oParentView.byId("Contract").getValue();
 
-        if (Partner.length < 10) {
-          var MissingLeadingZeros = 10 - Partner.length;
-          for (var i = 0; i < MissingLeadingZeros; i++) {
-            Partner = "0" + Partner;
-          }
+          // var Partner = oSelectedItem.mProperties.title;
+
+          // var sBalancePath = "/ZADOC_ROYALTIES_BALANCE";
+
+          // var oPartnerFilter = new sap.ui.model.Filter(
+          //   "Partner",
+          //   sap.ui.model.FilterOperator.EQ,
+          //   Partner
+          // );
+
+          // var oContractFilter = new sap.ui.model.Filter(
+          //   "Contract",
+          //   sap.ui.model.FilterOperator.EQ,
+          //   Contract
+          // );
+
+          //       if (Contract) filters.push(oContractFilter);
+
+          //       var oBalanceInput = oEvent.getSource().getParent().byId("Balance");
+
+          //       oModel.read(sBalancePath, {
+
+          //         filters: [oPartnerFilter],
+
+          //         success: function (oData, oResponse) {
+          //           if (oData.results[0])
+          //           oBalanceInput.setValue(oData.results[0].Balance);
+          //         }.bind(this),
+
+          //         error: function (oError) {
+          //           sap.m.MessageToast.show("Erro ao carregar os dados de transgenia");
+          //         },
+          //       });
         }
-
-        var sBalancePath = "/ZADOC_ROYALTIES_BALANCE";
-
-        var oPartnerFilter = new sap.ui.model.Filter(
-          "Partner",
-          sap.ui.model.FilterOperator.EQ,
-          Partner
-        );
-
-        var oContractFilter = new sap.ui.model.Filter(
-          "Contract",
-          sap.ui.model.FilterOperator.EQ,
-          Contract
-        );
-
-        var filters = [];
-
-        filters.push(oPartnerFilter);
-
-        if (Contract) filters.push(oContractFilter);
-
-        var oBalanceInput = oEvent.getSource().getParent().byId("Balance");
-        
-
-        oModel.read(sBalancePath, {
-          filters: filters,
-
-          success: function (oData, oResponse) {
-            if (oData.results[0])
-            oBalanceInput.setValue(oData.results[0].Balance);
-          }.bind(this),
-
-          error: function (oError) {
-            sap.m.MessageToast.show("Erro ao carregar os dados de transgenia");
-          },
-        });
-      }
-    },
-  };
-});
+      },
+    };
+  }
+);
